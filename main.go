0ab18a43386e8053706d88e70af8e7c62f1a2eb2
Lima <lmimsra07@gmail.com>
2020-01-26 23:59:12 +0900

    適当にアクティビティ取得する関数を使ってみた

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"github.com/google/go-github/github"
 	"github.com/joho/godotenv"
+	"golang.org/x/oauth2"
 	"os"
 )
 
@@ -16,5 +19,23 @@ func main() {
 
 	fmt.Println("process start")
 	fmt.Println("run environment is " + os.Getenv("ENV"))
+	getGithubUserInfo()
+}
 
+func getGithubUserInfo() {
+	var accessToken string = os.Getenv("GITHUB_PERSONAL_ACCESS_KEY")
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: accessToken})
+	client := github.NewClient(oauth2.NewClient(ctx, ts))
+	var res []*github.Event
+	res, _, _ = client.Activity.ListEventsPerformedByUser(ctx, "lmimsra", false, &github.ListOptions{
+		Page:    0,
+		PerPage: 0,
+	})
+
+	for i := range res {
+		fmt.Println("repoName: " + res[i].Repo.GetName() + "Create: " + res[i].CreatedAt.Local().String())
+	}
+
+	//fmt.Println(res[1])
 }
