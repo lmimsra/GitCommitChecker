c820fa062a1b9bd00aba3ee24493a997502e5911
Lima <lmimsra07@gmail.com>
2020-01-30 17:57:14 +0900

    処理を全体的に見直し

--- main.go
+++ main.go
@@ -15,14 +15,14 @@ import (
 )
 
 const (
-	LOCATION = "Asia/Tokyo"
-	USERNAME = "GoBot"
+	Location    = "Asia/Tokyo"
+	BotUserName = "git commit checker"
 )
 
 func init() {
-	loc, err := time.LoadLocation(LOCATION)
+	loc, err := time.LoadLocation(Location)
 	if err != nil {
-		loc = time.FixedZone(LOCATION, 9*60*60)
+		loc = time.FixedZone(Location, 9*60*60)
 	}
 	time.Local = loc
 }
@@ -39,9 +39,11 @@ func main() {
 	fmt.Println("run environment is " + os.Getenv("ENV"))
 	numOfActivity := getGithubUserInfo()
 
-	fmt.Println("num of activity" + strconv.Itoa(numOfActivity))
-	tweetCommit(numOfActivity)
-	postSlack(numOfActivity)
+	fmt.Println("num of activity　" + strconv.Itoa(numOfActivity))
+	postMessage := getPostComment(numOfActivity)
+	postTwitter(postMessage)
+	postSlack(postMessage)
+	fmt.Println("[INFO] finish commit check")
 }
 
 // 当日のアクティビティを取得する（privateのコミット、アクティビティも含む）
@@ -69,12 +71,13 @@ func getGithubUserInfo() int {
 	unixTime := now.Unix()
 	_, offset := now.Zone()
 	today := time.Unix((unixTime/86400)*86400-int64(offset), 0)
-	fmt.Println("toDay is " + today.String())
+	year, month, day := today.Date()
+	fmt.Printf("today is %v年 %v月 %v日\n", year, int(month), day)
 
 	var todayActivity []*github.Event
 	if err == nil {
 		for i := range res {
-			fmt.Println("repoName: " + res[i].Repo.GetName() + "Create: " + res[i].CreatedAt.Local().String())
+			fmt.Println("repoName: " + res[i].Repo.GetName() + " ActivityTime: " + res[i].CreatedAt.Local().String())
 
 			if res[i].CreatedAt.Local().After(today) {
 				todayActivity = append(todayActivity, res[i])
@@ -90,26 +93,23 @@ func getGithubUserInfo() int {
 	for i := range todayActivity {
 		fmt.Println("repoName: " + todayActivity[i].Repo.GetName() + "Create: " + res[i].CreatedAt.Local().String())
 	}
-
-	//println("commits")
-	//for i := range resCommits {
-	//	fmt.Println("name is " + resCommits[i].Author.GetName() + " message is " + resCommits[i].Commit.GetMessage())
-	//}
-
 	return len(todayActivity)
 }
 
 // アクティビティ数をTwitterに投稿
-func tweetCommit(numOfActivity int) {
+func postTwitter(postMessage string) {
 	apiKey := os.Getenv("TWITTER_API_KEY")
 	apiSecret := os.Getenv("TWITTER_API_SECRET")
 	accessToken := os.Getenv("TWITTER_ACCESS_TOKEN")
 	accessTokenSecret := os.Getenv("TWITTER_ACCESS_TOKEN_SECRET")
 	api := anaconda.NewTwitterApiWithCredentials(accessToken, accessTokenSecret, apiKey, apiSecret)
+	targetTwitterAccount := "@" + os.Getenv("TWITTER_TARGET_USER_ID")
+	message := BotUserName + " posted message\n" + targetTwitterAccount + "\n" + postMessage
 
-	tweet, err := api.PostTweet("tweet by Go! local time is "+time.Now().String(), url.Values{})
+	tweet, err := api.PostTweet(message, url.Values{})
 	if err != nil {
 		fmt.Println("[ERROR] Slack post fail")
+		fmt.Println(err)
 		os.Exit(1)
 	} else {
 		fmt.Println("[INFO] tweet success!  post ID:" + tweet.IdStr)
@@ -117,22 +117,22 @@ func tweetCommit(numOfActivity int) {
 }
 
 // アクティビティ数をSlackに投稿
-func postSlack(numOfActivity int) {
+func postSlack(postMessage string) {
 	webHookURL := os.Getenv("SLACK_WEB_HOOK_URL")
-	field1 := slack.Field{Title: "Message", Value: "テスト送信"}
-	field2 := slack.Field{Title: "AnythingKey", Value: "AnythingValue"}
-
+	field1 := slack.Field{Title: "コミット数のお知らせ", Value: postMessage}
 	attachment := slack.Attachment{}
-	attachment.AddField(field1).AddField(field2)
+	attachment.AddField(field1)
 	color := "good"
 	attachment.Color = &color
+	targetSlackAccount := fmt.Sprintf("<@%s>", os.Getenv("SLACK_TARGET_USER_ID"))
+
 	payload := slack.Payload{
 		Parse:       "",
-		Username:    "",
+		Username:    BotUserName,
 		IconUrl:     "",
 		IconEmoji:   "",
 		Channel:     "",
-		Text:        "テキストのテスト",
+		Text:        targetSlackAccount,
 		LinkNames:   "",
 		Attachments: []slack.Attachment{attachment},
 		UnfurlLinks: false,
@@ -142,8 +142,23 @@ func postSlack(numOfActivity int) {
 	err := slack.Send(webHookURL, "", payload)
 	if err != nil {
 		fmt.Println("[ERROR] Slack post fail")
+		fmt.Println(err)
 		os.Exit(1)
 	} else {
 		fmt.Println("[INFO] slack post success!")
 	}
+}
+
+// 送るメッセージ内容をアクティビティ数で変更
+func getPostComment(numOfActivity int) string {
+	var message string
+	switch {
+	case numOfActivity == 0:
+		message = "今日のコミット数0！？\nまずいですよ！！！"
+	case numOfActivity >= 1 && numOfActivity < 5:
+		message = fmt.Sprintf("今日はコミット%d件\nまぁまぁやってはいるようですね", numOfActivity)
+	case numOfActivity >= 5:
+		message = fmt.Sprintf("今日はコミット%d件も！\nよろしい！", numOfActivity)
+	}
+	return message
 }
